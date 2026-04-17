c1d56041da8bc1182a9829f1c18033cf27f73122
DVagner02 <danielvagner05@gmail.com>
2026-04-18 00:51:05 +0200

    Connect vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+import { Analytics } from '@vercel/analytics/react'
 import App from './app/App'
 import './styles/index.css'
+import './i18n'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
